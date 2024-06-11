df1c936029df4592a99bd7edf50d67da051ea34f
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-06-11 14:20:28 +0700

    login level

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,6 +5,7 @@ class Auth extends CI_Controller
 {
 	public function login()
 	{
+		check_already_login();
 		$this->load->view('login');
 	}
 	public function process()
@@ -33,5 +34,12 @@ class Auth extends CI_Controller
 			}
 
 		} 
+	}
+
+	public function logout()
+	{
+		$params = array('userid', 'level');
+		$this->session->unset_userdata($params);
+		redirect('auth/login');
 	}
 }
