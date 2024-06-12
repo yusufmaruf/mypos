7cae450d3b29bcc32da7ee07d3d2bc7f86036381
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-06-12 14:50:16 +0700

    category with flash data

---
+++ application/models/Category_m.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_m extends CI_Model {
+
+    public function get($id = null){
+        $this->db->from('p_category');
+        if($id != null) {
+            $this->db->where('category_id', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function add($post){
+        $params = [
+            'name' => $post['name'],
+        ];
+        $this->db->insert('p_category', $params);
+    }
+
+    public function edit($post){
+        $params = [
+            'name' => $post['name'],
+            'updated' => date('Y-m-d H:i:s')
+        ];
+        $this->db->where('category_id', $post['category_id']);
+        $this->db->update('p_category', $params);
+    }
+
+    public function del($id){
+        $this->db->where('category_id', $id);
+        $this->db->delete('p_category');
+    }
+        
+}
---
+++ application/views/product/category/category_data.php
@@ -0,0 +1,56 @@
+<section class="content-header">
+	<h1>User
+		<small> Category </small>
+	</h1>
+	<ol class="breadcrumb">
+		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li class="active">Category Barang</li>
+	</ol>
+</section>
+
+<section class="content">
+    <?php $this->view('messages') ?>
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Data Category</h3>
+            <div class="pull-right">
+                <a href="<?=site_url('category/add')?>" class="btn btn-primary btn-flat">
+                    <i class="fa fa-plus"></i> Create
+                </a>
+            </div>
+        </div>
+        <div class="box-body table-responsive">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>name</th>    
+                        <th>action</th>  
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($row->result() as $key => $data) { ?>                        
+                        <tr>
+                            <td><?php echo $key+1 ?></td>
+                            <td><?php echo $data->name ?></td>
+                            <td width="160px" class="text-center">
+                                <a href="<?php echo site_url('category/del/'.$data->category_id) ?>" onclick="return confirm('Are you sure you want to delete this data?')" class="btn btn-danger btn-xs">
+                                    <i class="fa fa-trash"></i> Delete
+                                </a> 
+                                <a href="<?php echo site_url('category/edit/'.$data->category_id) ?>" class="btn btn-warning btn-xs">
+                                    <i class="fa fa-pencil"></i> Edit
+                                </a> 
+                                                          
+                            </td>
+                            
+                        </tr>
+                    <?php } ?>
+
+                   
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</section>
