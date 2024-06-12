57a5843b5f8f2c43594539b6477e7e7dbf5dab63
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-06-12 10:23:29 +0700

    akses role level

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,13 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
     function __construct(){
         parent::__construct();
+        check_not_login();
+        check_admin();
         $this->load->model('user_m');
         $this->load->library('form_validation');
+
     }
 
 	public function index()
 	{
-		check_not_login();
         $data['row'] = $this->user_m->get();
 		$this->template->load('template', 'user/user_data', $data);
 	}
